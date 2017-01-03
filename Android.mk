682627ecc6c09e887fc03dae053450f6e88d22bc
sun.ao <sunao2002002@gmail.com>
2017-01-03 09:53:30 +0800

    first commit

---
+++ Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := ifstat.c \
+				drivers.c \
+				data.c 
+
+
+LOCAL_CFLAGS += -DHAVE_CONFIG_H 
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE := ifstat
+LOCAL_STATIC_LIBRARIES += libc \
+
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+include $(BUILD_EXECUTABLE)
